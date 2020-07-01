0aa30d695315428ff9b797b2a207eff63ff8d999
ductranprof99 <ducprofessorx@gmail.com>
2020-07-01 16:32:37 +0700

    add models

---
+++ Client/FoodCourt.App/FoodCourt.App/Models/TotalPrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodCourt.Models
+{
+    public class TotalPrice
+    {
+        public double subTotal { get; set; }
+    }
+}
