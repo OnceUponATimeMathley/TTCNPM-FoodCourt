a4486028651f3d7f048c443ba2568bc7dd2d42e0
= <=>
2020-07-02 20:47:49 +0700

    add simple menu tab

--- Client/FoodCourt.App/FoodCourt.App.Android/obj/Debug/90/android/environment.x86_64.s
+++ Client/FoodCourt.App/FoodCourt.App.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"459720df-1800-4fbd-9e32-b12118dd7490"
+	.asciz	"d95db17e-b896-4b32-8a4b-8c2e0423ae30"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- Client/FoodCourt.App/FoodCourt.App/Pages/HomePage.xaml.cs
+++ Client/FoodCourt.App/FoodCourt.App/Pages/HomePage.xaml.cs
@@ -16,5 +16,17 @@ namespace FoodCourt.App.Pages
         {
             InitializeComponent();
         }
+
+        private async void ImgMenu_OnTapped(object sender, EventArgs e)
+        {
+            GridOverlay.IsVisible = true;
+            await SlMenu.TranslateTo(0, 0, 400, Easing.Linear);
+        }
+
+        private async void TapCloseMenu_OnTapped(object sender, EventArgs e)
+        {
+            await SlMenu.TranslateTo(-250, 0, 400, Easing.Linear);
+            GridOverlay.IsVisible = false;
+        }
     }
 }
